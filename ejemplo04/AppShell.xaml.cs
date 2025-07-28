08d3e8a797ec5e32278edb9aa0d55c59088dbb0c
50371 <50371@LAPTOP-DE-ORTIX>
2025-07-28 17:15:26 -0600

    Agregar archivos de proyecto.

---
+++ ejemplo04/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ejemplo04
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ejemplo04/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace ejemplo04
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}
